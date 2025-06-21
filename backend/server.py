bb5b3f8661814aec7b02d9e484f8a5594dcfdc13
digitalyoshixi <digitalyoshixi@proton.me>
2025-06-21 10:51:21 -0400

    server boilerplate

---
+++ backend/server.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+	return {"Hello" : "World"}
+
+# items = []
+# @app.post("/items")
+# def create_item(item : str):
+# 	items.append(item)
+# 	return items
+# 	
+# @app.get("/items/{item_id}")
+# def get_item(item_id : int) -> str:
+# 	item = items[item_id]
+# 	return item
