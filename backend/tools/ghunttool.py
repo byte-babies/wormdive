5f16258f5d2c486c9ad926dfd222d1fc10f9d021
digitalyoshixi <digitalyoshixi@proton.me>
2025-06-21 18:52:36 -0400

    new backend

--- backend/tools/ghunttool.py
+++ backend/tools/ghunttool.py
@@ -3,22 +3,22 @@ from ghunt.objects.base import GHuntCreds
 import subprocess
 import asyncio
 
-print("Enter email: ", end="")
-email = input()
 
-async def run(cmd):
-    proc = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
+def find_email(email):
+    async def run(cmd):
+        proc = await asyncio.create_subprocess_shell(
+            cmd,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE)
+    
+        stdout, stderr = await proc.communicate()
+    
+        print(f'[{cmd!r} exited with {proc.returncode}]')
+        if stdout:
+            return stdout.decode()
+        if stderr:
+            return stderr.decode()
+    rawoutput = asyncio.run(run(f'ghunt email {email}'))
+    return 
 
-    stdout, stderr = await proc.communicate()
-
-    print(f'[{cmd!r} exited with {proc.returncode}]')
-    if stdout:
-        return stdout.decode()
-    if stderr:
-        return stderr.decode()
-
-output = asyncio.run(run(f'ghunt email {email}'))
-print(output)
+print(find_email(input()))
