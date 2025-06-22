da1073aad69a674e30a83cf992a84fa051d092f9
rizfol <blahblabh@example.com>
2025-06-21 20:57:19 -0400

    gemini reading json stuff

--- backend/helpers/gemini.py
+++ backend/helpers/gemini.py
@@ -9,7 +9,8 @@ GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 
 client = genai.Client(api_key=GEMINI_API_KEY)
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
-)
-print(response.text)
+with open('everything.json', 'r') as f:
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents="You are an OSINT investigator, try to infer any connections betweens nodes and summarize it to the user"+f.read()
+    )
+    # display the response somewhere on the frontend
